85b534e86abe04976e70965dab6cd9f03f7c3702
Denali Balser <denali.balser167@gmail.com>
2019-10-09 12:45:50 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  def initialize(name, weight, height)
+    @name = params[name]
+    @weight = params[weight]
+    @height = params[height]
+  end 
 end
